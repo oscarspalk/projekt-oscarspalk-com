2cfb9e566e6cc48833a8cfab83d61a96672cc0ba
Olle <oscar.gaardsted.spalk@gmail.com>
2021-11-26 23:42:54 +0100

    added dark mode, nice

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
-import {GeistProvider, CssBaseline} from '@geist-ui/react'
+import {GeistProvider, CssBaseline, Button} from '@geist-ui/react'
+import {useState} from 'react'
+import styles from '../styles/Home.module.css'
+import { Moon, Sun } from '@geist-ui/react-icons'
+
 function MyApp({ Component, pageProps }) {
+  const [themeType, setThemeType] = useState(false);
   return(
-    <GeistProvider themeType="light">
+    <GeistProvider themeType={themeType ? 'dark' : 'light'}>
+      <Button iconRight={themeType ? <Moon /> : <Sun />} auto scale={2/3} px={0.6} onClick={() => setThemeType(!themeType)} className={styles.fixed} />
       <CssBaseline>
       <Component {...pageProps} />
       </CssBaseline>
